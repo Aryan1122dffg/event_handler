f41298b395d4abc0e0a65bd4c6f70429a5faf1dd
Aryan <aryaningle1234.i@gmail.com>
2024-09-02 22:25:46 +0530

    ji0

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+return (
+	<div>
+	<footer> 
+		<p>@ copyright of event planner to know more visit event PLanner</p> 
+	</footer>
+</div>	
+);
+};
+
+export default Footer; 
